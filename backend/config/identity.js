1b385c3d1df87eb374efd01ea6851313d54d107f
Sitanshu Misra <misrasitanshu4@gmail.com>
2026-04-24 19:22:36 +0530

    fix: update identity credentials

--- backend/config/identity.js
+++ backend/config/identity.js
@@ -1,5 +1,5 @@
 module.exports = {
-  user_id: "sitanshumisra_ddmmyyyy",
-  email_id: "sm1234@srmist.edu.in",
-  college_roll_number: "RA2211003010XXX"
+  user_id: "sitanshumisra_17012005",
+  email_id: "sm3421@srmist.edu.in",
+  college_roll_number: "RA2311032010022"
 };
